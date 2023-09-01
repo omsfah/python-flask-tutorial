4d3918b8d962c0a8162638165297a8d00816f9f3
Omsfah skole-pc <olafhafsmo@gmail.com>
2023-09-02 01:12:21 +0200

    Barebone, working on css and button definitions

--- FlaskMarket/main.py
+++ FlaskMarket/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template('dashboard.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True) #tillater å kjøre "direkte" med debug mode (ved filendringer trenger man ikke restarte appen)
