0818aa231581db98d894cbcf3dd721203efbefb6
Omsfah skole-pc <olafhafsmo@gmail.com>
2023-09-01 21:27:06 +0200

    Cleanup, startover

---
+++ FlaskMarket/main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+app = Flask(__name__)
+
+
+@app.route('/')
+def home_page():
+    return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    app.run(debug=True) #tillater å kjøre "direkte" med debug mode (ved filendringer trenger man ikke restarte appen)
